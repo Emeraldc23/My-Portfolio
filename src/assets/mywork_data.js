10f8a3922fd900d7ab23bf71c4e04efe793274d9
Maduagwu Martins <maduagwumartins16@gmail.com>
2025-08-29 16:58:55 +0100

    added new website

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -6,25 +6,26 @@ import project5_img from "../assets/image/project_5.svg";
 import project6_img from "../assets/image/project_6.svg";
 import EmeralCity from "../assets/image/Emeraldcity.png";
 import foodRecipe from "../assets/image/foodRecipe.png";
+import chefEmerald from "../assets/image/recipeImage.png";
 
 const mywork_data = [
   {
     w_no: 1,
-    w_name: "Web design",
+    w_name: "Emerald City",
     w_img: EmeralCity,
     w_url: "https://emerald-city-3wuz.vercel.app/",
   },
   {
     w_no: 2,
-    w_name: "Web design",
+    w_name: "Food Generator",
     w_img: foodRecipe,
     w_url: "https://react-food-recipe-fawn.vercel.app/",
   },
   {
     w_no: 3,
-    w_name: "Web design",
-    w_img: project3_img,
-    w_url: "",
+    w_name: "Chef Emerald",
+    w_img: chefEmerald,
+    w_url: "https://chef-emerald.vercel.app/",
   },
   {
     w_no: 4,
